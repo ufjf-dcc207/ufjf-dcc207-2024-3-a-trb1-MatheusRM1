2a514f9df5090e4caee885889a9c5d6c4a74d6ee
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2024-12-10 16:50:10 -0300

    cria bloco

---
+++ src/Bloco.tsx
@@ -0,0 +1,11 @@
+import './Bloco.css';
+
+export default function Bloco() {
+  return (
+    <div className="container">
+      <div className="bloco">
+          Pergunta
+      </div>
+    </div>
+  );
+}
