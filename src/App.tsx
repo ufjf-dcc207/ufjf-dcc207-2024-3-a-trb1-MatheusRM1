502a051371d91c43598c757e629c4afa7ef9463f
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2024-12-16 17:01:17 -0300

    troca ts para json

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import Alternativa from "./Alternativa";
-import "./App.css";
-import Questoes from "./assets/dados";
+import questoes from "./assets/dados.json"
 import Bloco from "./Bloco";
+import './App.css'
 
 export default function App() {
   return (
     <div className="app">
       <h1 className="titulo">Quiz Red Dead Redemption 2</h1>
-      <p className="respostas">Respostas Certas: {Questoes.length}/{Questoes.length} (100%)</p>
-      {Questoes.map((questao) => (
-        <Bloco key={questao[0]} pergunta={questao[0]}>
-        {questao[1].map((alter) => (
-          <Alternativa key={alter[0]} resposta={alter[0]} certo={alter[1]}/>
+      <p className="respostas">Respostas Certas: {questoes.Questoes.length}/{questoes.Questoes.length}(100%)</p>
+      {questoes.Questoes.map((questao) => (
+        <Bloco key={questao.pergunta} pergunta={questao.pergunta}>
+        {questao.alternativas.map((alter) => (
+          <Alternativa key={alter.resposta} resposta={alter.resposta} certo={alter.certo}/>
         ))}
         </Bloco>
       ))}
